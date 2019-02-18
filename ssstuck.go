fa0d31f0af9f21d10a58c3fcaf0ee697a2566ebd
Mike <hey@mike.ee>
2019-02-18 13:06:48 +0000

    Implement server entrypoint and ssh config structure

--- ssstuck.go
+++ ssstuck.go
@@ -1,7 +1,13 @@
 package ssstuck
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"golang.org/x/crypto/ssh"
 )
 
 // Config is the basic structure to hold required startup variables
@@ -15,4 +21,47 @@ func CheckConfig(config Config) error {
 		return fmt.Errorf("port(%d) please specify a port between 1-65535", config.Port)
 	}
 	return nil
+}
+
+// Serve is the main entrypoint to the program
+func Serve(config Config) {
+	err := CheckConfig(config)
+	if err != nil {
+		log.Panicf("invalid setting: %s", err)
+	}
+	serverConfig := getServer()
+}
+
+func getServer() ssh.ServerConfig {
+	serverConfig := &ssh.ServerConfig{
+		PublicKeyCallback: authKey,
+		PasswordCallback:  authPassword,
+	}
+
+	keyBytes, _ := rsa.GenerateKey(rand.Reader, 4096)
+	key, _ := ssh.NewSignerFromSigner(keyBytes)
+	serverConfig.AddHostKey(key)
+
+	return *serverConfig
+}
+
+func authKey(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+	log.WithFields(log.Fields{
+		"type":   "public key",
+		"ip":     conn.RemoteAddr(),
+		"key":    key,
+		"client": string(conn.ClientVersion()),
+	}).Info("connection attempt")
+	return nil, fmt.Errorf("invalid credentials")
+}
+
+func authPassword(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+	log.WithFields(log.Fields{
+		"type":     "user-password",
+		"ip":       conn.RemoteAddr(),
+		"user":     string(conn.User()),
+		"password": string(password),
+		"client":   string(conn.ClientVersion()),
+	}).Info("connection attempt")
+	return nil, fmt.Errorf("invalid credentials")
 }
